3392dfd54c0820c21af46d726cb6d8bcc3468836
tejas123 <jasani.tejas@gmail.com>
2014-04-05 11:57:28 +0530

    Final
    Final

---
+++ Assets/Scripts/Lighting.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lighting : MonoBehaviour 
+{
+	private bool isMousePressed; // Used to determine whether mouse buttono is pressed or not
+	void Start () 
+	{
+		isMousePressed = false;
+	}
+
+	void Update ()
+	{
+		if(Input.GetMouseButtonDown(0))
+			isMousePressed = true;
+		else if(Input.GetMouseButtonUp(0))
+			isMousePressed = false;
+
+		// Following lines move the light at mouse position
+		if(isMousePressed)
+		{
+			Vector3 lightPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			lightPos.z = -1;
+			transform.position = lightPos;
+		}
+	}
+}
